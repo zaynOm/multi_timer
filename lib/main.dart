1562bbf463663207b6035a6d6a5e50474ce09c96
zaynom <ouaziz0626@gmail.com>
2025-04-14 22:20:32 +0100

    feat: integrate Firebase services including Crashlytics and Analytics

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,9 @@
+import 'dart:ui';
+
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/material.dart';
+import 'package:multi_timer/firebase_options.dart';
 import 'package:provider/provider.dart';
 
 import 'app.dart';
@@ -8,6 +13,16 @@ import 'services/timer_service.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+
+  // Initialize Firebase
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  // Enable Crashlytics
+  FlutterError.onError = FirebaseCrashlytics.instance.recordFlutterFatalError;
+  PlatformDispatcher.instance.onError = (error, stack) {
+    FirebaseCrashlytics.instance.recordError(error, stack, fatal: true);
+    return true;
+  };
 
   // Initialize all services
   await ServiceLocator.setup();
